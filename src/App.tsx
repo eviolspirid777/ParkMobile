428efc440615109b153efefe46dd3d8126af537b
EvilSpirit_ <93467283+eviolspirid777@users.noreply.github.com>
2024-11-09 23:31:19 +0300

    добавлены популярные товары и категории

--- src/App.tsx
+++ src/App.tsx
@@ -1,101 +1,16 @@
 import "./App.scss";
-import img from "./assets/Logo.png";
+import {Header} from "./Pages/Header/Header.tsx";
+import { PopularItems } from "./Pages/PopularItems/PopularItems.tsx";
+import {SwiperList} from "./Pages/Swiper/Swiper.tsx";
+import {UnderSwiperCards} from "./Pages/UnderSwiperCards/UnderSwiperCards.tsx";
 
 export const App = () => {
-  const navLinks = [
-    "Mac",
-    "Iphone",
-    "Ipad",
-    "Watch",
-    "Airpods",
-    "TV и Дом",
-    "Аксессуары",
-    "Доставка",
-  ];
-
   return (
     <>
-      <header
-        style={{
-          position: "sticky",
-          display: "grid",
-          gridTemplateColumns: "1fr 1fr 1fr",
-          width: "100%",
-          height: "50px",
-          backgroundColor: "#454545",
-        }}
-      >
-        <img
-          src={img}
-          style={{
-            justifySelf: "center",
-            alignSelf: "center",
-          }}
-        />
-        <nav
-          style={{
-            display: "flex",
-            flexFlow: "row nowrap",
-            gap: "10px",
-            alignSelf: "center",
-            color: "white",
-          }}
-        >
-          {navLinks.map((el) => (
-            <a
-              key={el}
-              style={{
-                fontFamily: "SF-Pro-Display-Regular_light",
-                fontSize: "0.9rem",
-              }}
-            >
-              {el}
-            </a>
-          ))}
-        </nav>
-        <nav
-          style={{
-            alignSelf: "center",
-            justifySelf: "center",
-            display: "flex",
-            flexFlow: "row nowrap",
-            alignItems: "center",
-            gap: "10px",
-          }}
-        >
-          <i
-            className="fa-solid fa-magnifying-glass fa-xs"
-            style={{
-              color: "white",
-            }}
-          />
-          <div
-            style={{
-              display: "flex",
-              flexFlow: "row nowrap",
-              alignItems: "center",
-              gap: "3px",
-            }}
-          >
-            <span
-              style={{
-                backgroundColor: "#3c3c3c",
-                borderRadius: "1000px",
-                padding: "2px 6px",
-                color: "white",
-              }}
-            >
-              0
-            </span>
-            <i
-              className="fa-regular fa-bag-shopping fa-xs"
-              style={{
-                color: "white",
-              }}
-            />
-          </div>
-        </nav>
-      </header>
+      <Header />
+      <SwiperList />
+      <UnderSwiperCards />
+      <PopularItems />
     </>
   );
 };
---
+++ src/Pages/Header/Header.tsx
@@ -0,0 +1,54 @@
+import img from "../../assets/Logo.png";
+import styles from "./Header.module.scss";
+import {useState} from "react";
+
+export const Header = () => {
+    const navLinks = [
+        "Mac",
+        "Iphone",
+        "Ipad",
+        "Watch",
+        "Airpods",
+        "TV и Дом",
+        "Аксессуары",
+        "Доставка",
+    ];
+    
+    const [itemInBucket, setItemInBucket] = useState<number>(1);
+
+    return (
+        <header
+          className={styles["header"]}
+        >
+          <img
+            src={img}
+            className={styles["logo"]}
+          />
+          <nav
+            className={styles["nav-bar"]}
+          >
+            {navLinks.map((el) => (
+              <a
+                key={el}
+                className={styles["nav-item"]}
+              >
+                {el}
+              </a>
+            ))}
+          </nav>
+          <nav
+            className={styles["nav-bucket-search"]}
+          >
+            <i className="fa-thin fa-magnifying-glass fa-lg"></i>
+            <div
+              className={styles["nav-bucket-search-shop-block"]}
+            >
+              <span>
+                {itemInBucket}
+              </span>
+              <i className="fa-sharp fa-thin fa-bag-shopping fa-lg"></i>
+            </div>
+          </nav>
+        </header>
+    )
+}
