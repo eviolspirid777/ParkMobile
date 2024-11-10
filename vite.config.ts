d2a329380d9c1d9fe2e559874959ce95134a7ed6
eviolspirid777 <eviolspirid@gmail.com>
2024-11-10 23:13:56 +0300

    Добавлен gap у header изменен vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Укажите папку для билда
+    rollupOptions: {
+      input: resolve(__dirname, 'index.html'), // Используем resolve из path
+    }
+  }
 })
